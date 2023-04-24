9d3705e3ab7cd344727769c0ad219016950ee190
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2023-04-24 12:05:58 +0900

    use useEffect and useCallBack to trigger the fetch function whenever the component is loaded for the first time and whenever the button is clicked
    First, instead of calling the function directly
    inside the component, is good to create a useEffect
    hook to call the function. Leaving the dependencies
    array empty, will trigger the function the first
    time the component is loaded.
    
    However, it's a good practice to add everything used
    inside the useEffect hook in the dependencies array.
    But by doing that, you might create a infinite loop
    because every time the component is loaded, the function
    pointer changes because a new variable is assigned,
    a function in JS is an object
    
    The best practice is to add the function inside an
    anonymous functions inside a useCallBack hook.
    That way, the function is created and is stored inside a
    useCallBack hook, so it won't re-render every time
    the component is loaded without a reason

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -14,40 +14,47 @@ function App() {
   //state for error handling
   const [error, setError] = useState(null)
 
-  async function fetchMoviesHandler() {
-    setIsLoading(true)
-    setError(null)
+  const fetchMoviesHandler = useCallback( async () => { 
+  setIsLoading(true)
+  setError(null)
 
-    try {
-      const response = await fetch('https://swapi.dev/api/films/', {
-        method: 'GET'
-      })
+  try {
+    const response = await fetch('https://swapi.dev/api/films/', {
+      method: 'GET'
+    })
 
 
-      if (!response.ok) {
-        //throwing a new error
-        throw new Error('Something went Wrong')
-      }
-
-      const data = await response.json()
-
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
-      setMovies(transformedMovies)
-
-    } catch (err) {
-      setError(err.message)
-
+    if (!response.ok) {
+      //throwing a new error
+      throw new Error('Something went Wrong')
     }
 
-    setIsLoading(false)
+    const data = await response.json()
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }
+    })
+    setMovies(transformedMovies)
+
+  } catch (err) {
+    setError(err.message)
+
   }
+
+  setIsLoading(false)
+}, [])
+
+//using useEffect to fetch data
+useEffect(() => {
+
+  //it's a good practice to add all data used in the useEffect in the dependencies array
+  fetchMoviesHandler()
+}, [fetchMoviesHandler])
 
   return (
     <React.Fragment>
